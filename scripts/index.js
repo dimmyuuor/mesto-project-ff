27ac8ef34d376252a5c62ac746b030d517a2f1e0
dimmyuuor <dmitry8878@yandex.ru>
2023-11-26 13:13:12 +0500

    mesto

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,24 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+
+
+
+initialCards.forEach(function (card) {
+  const template = document.querySelector('#card-template').content;
+  const templateElement = template.querySelector('.places__item').cloneNode(true);
+  const cardElement = document.querySelector('.places__list');
+  templateElement.querySelector('.card__image').src = card.link;
+  templateElement.querySelector('.card__image').alt = card.name;
+  templateElement.querySelector('.card__title').textContent = card.name;
+  cardElement.append(templateElement);
+});
+
+
+document.querySelector('body').onclick = function(e) {
+  if(e.target.className != 'card__delete-button') return
+  let item = e.target.closest('.card');
+  item.remove()
+}
