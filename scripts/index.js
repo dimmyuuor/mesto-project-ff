bb035b4b2d5b05d3cb27f0cdf39d6ef71a188756
dimmyuuor <dmitry8878@yandex.ru>
2023-11-27 19:35:47 +0500

    mesto

--- scripts/index.js
+++ scripts/index.js
@@ -10,20 +10,25 @@
 
 
 
+const cardElement = document.querySelector('.places__list');
+const template = document.querySelector('#card-template').content;
 
-
-initialCards.forEach(function (card) {
-  const template = document.querySelector('#card-template').content;
+function createCard(card, deleteCard) {
   const templateElement = template.querySelector('.places__item').cloneNode(true);
-  const cardElement = document.querySelector('.places__list');
   templateElement.querySelector('.card__image').src = card.link;
   templateElement.querySelector('.card__image').alt = card.name;
   templateElement.querySelector('.card__title').textContent = card.name;
-  cardElement.append(templateElement);
-});
-
-document.querySelector('body').onclick = function(e) {
-  if(e.target.className != 'card__delete-button') return
-  const item = e.target.closest('.card');
-  item.remove()
+  const deleteButton = templateElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', deleteCard);
+  return templateElement;
 }
+
+function deleteCard(event) {
+  const card = event.target.closest('.card')
+  card.remove();
+}
+
+initialCards.forEach((card) => {
+  const place = createCard(card, deleteCard);
+  cardElement.append(place);
+});
